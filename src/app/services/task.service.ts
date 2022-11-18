6365f127c8d1993fb31c4a587dd21901ace0e075
Miraj Aryal <mirajaryal@gmail.com>
2022-11-18 15:17:40 +1100

    adding error handling

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../Task';
-import { Observable } from 'rxjs';
+import { catchError, Observable, ObservableInput, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
@@ -14,23 +14,32 @@ const httpOptions = {
 })
 export class TaskService {
   private baseUrl = 'http://localhost:5000/tasks';
+  handleError: (err: any, caught: Observable<Task>) => ObservableInput<any>;
 
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.baseUrl);
+    return this.http
+      .get<Task[]>(this.baseUrl)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 
   addTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.baseUrl, task, httpOptions);
+    return this.http
+      .post<Task>(this.baseUrl, task, httpOptions)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
   deleteTask(task: Task): Observable<Task> {
     const url = this.baseUrl + '/' + task.id;
-    return this.http.delete<Task>(url);
+    return this.http
+      .delete<Task>(url)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 
   toggleReminder(task: Task): Observable<Task> {
     const url = this.baseUrl + '/' + task.id;
-    return this.http.put<Task>(url, task, httpOptions);
+    return this.http
+      .put<Task>(url, task, httpOptions)
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 }
